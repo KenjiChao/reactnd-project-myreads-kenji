481f0cd5b638c781c69d6883b769f650a35dfa4a
Kenji Chao <cchao1@alumni.cmu.edu>
2017-08-06 19:09:37 -0700

    feat: Load book data from server

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import BookShelves from './BookShelves';
 import Search from './Search';
 
 class BooksApp extends React.Component {
-  state = {};
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    });
+  }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <BookShelves/>
+          <BookShelves
+            books={this.state.books}
+          />
         )}/>
         <Route exact path="/search" render={() => (
           <Search/>
