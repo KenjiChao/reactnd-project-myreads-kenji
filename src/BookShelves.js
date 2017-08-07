87262e4001b8ca9d34327b7bf69041d9a5a67a1e
Kenji Chao <cchao1@alumni.cmu.edu>
2017-08-07 00:53:19 -0700

    style: Change text, color, and font-family

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -24,7 +24,7 @@ class BookShelves extends Component {
     return (
       <div className="list-books">
         <div className="list-books-title">
-          <h1>MyReads</h1>
+          <h1>Kenji's Reading List</h1>
         </div>
         <div className="list-books-content">
           <div>
